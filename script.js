948d0fb981b2ae81e7b007dd5a10b9385c8deafb
user name <user email>
2022-11-02 20:50:43 -0700

    Initial commit

--- script.js
+++ script.js
@@ -2,6 +2,42 @@ window.addEventListener("load", function(){
   // Canvas setup
   const canvas = document.getElementById('canvas1');
   const ctx = canvas.getContext('2d');
-  canvas.width = 500;
+  canvas.width = 1500;
   canvas.height = 500;
-})
+
+  class InputHandler {
+
+  }
+
+  class Projectile {
+
+  }
+
+  class Particle {
+
+  }
+
+  class Player {
+
+  }
+
+  class Enemy {
+
+  }
+
+  class Layer {
+
+  }
+  
+  class Background {
+
+  }
+
+  class UI {
+
+  }
+
+  class Game {
+
+  }
+});
