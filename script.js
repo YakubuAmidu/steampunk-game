e67c9457689afaa228114fcb68685689ab3ca38c
user name <user email>
2022-11-02 21:17:02 -0700

    Initial commit

--- script.js
+++ script.js
@@ -1,43 +1,64 @@
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
   // Canvas setup
-  const canvas = document.getElementById('canvas1');
-  const ctx = canvas.getContext('2d');
+  const canvas = document.getElementById("canvas1");
+  const ctx = canvas.getContext("2d");
   canvas.width = 1500;
   canvas.height = 500;
 
-  class InputHandler {
+  class InputHandler {}
 
-  }
+  class Projectile {}
 
-  class Projectile {
-
-  }
-
-  class Particle {
-
-  }
+  class Particle {}
 
   class Player {
+    constructor(game) {
+      this.game = game;
+      this.width = 120;
+      this.height = 190;
+      this.x = 20;
+      this.y = 100;
+      this.speedY = 0;
+    }
 
+    update() {
+      this.y += this.speedY;
+    }
+
+    draw(context) {
+      context.fillRect(this.x, this.y, this.width, this.height);
+    }
   }
 
-  class Enemy {
+  class Enemy {}
 
-  }
+  class Layer {}
 
-  class Layer {
+  class Background {}
 
-  }
-  
-  class Background {
-
-  }
-
-  class UI {
-
-  }
+  class UI {}
 
   class Game {
+    constructor(width, height) {
+      this.width = width;
+      this.height = height;
+      this.player = new Player(this);
+    }
 
+    update() {
+      this.player.update();
+    }
+
+    draw(context) {
+      this.player.draw(context);
+    }
+  }
+
+  const game = new Game(canvas.width, canvas.height);
+  // Animation loop
+  function animate(){
+    game.update();
+    game.draw(ctx);
+    requestAnimationFrame(animate);
   }
 });
